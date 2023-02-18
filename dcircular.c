3716c34246b4924af5e8f3bc9dd1e1d8707afd9a
jack_cizon <105433087+infinityjack@users.noreply.github.com>
2023-02-18 16:34:12 +0800

    Add files via upload

---
+++ dcircular.c
@@ -0,0 +1,1 @@
+#include"dcircular.h"
---
+++ dcircularclient.c
@@ -0,0 +1,54 @@
+#include"dcircular.h"
+
+
+circular(int);
+
+circular(char);
+
+circular(float);
+
+circular(double);
+
+circular(string);
+
+
+int main(int argc, char const *argv[])
+{
+    int_circular* ic = int_new();
+
+    int_push(ic, 1);
+    int_append(ic, 2);
+    //int_append(ic, 3);
+    //int_push(ic, 0);
+
+    //int_insert(ic, 0, -1);
+    //int_insert(ic, 5, 4);
+    //int_insert(ic, 3, 666);  
+
+    int_pop(ic);
+
+    int_pop(ic);
+    
+    int_append(ic, 9);
+
+    int_append(ic, 11);
+
+    int_insert(ic, 1, 999);
+
+    int_delete(ic, 1);
+
+    int_delete(ic, 0);
+    int_delete(ic, 0);
+
+    int_push(ic, 888);
+
+    int_remove(ic, 888);
+
+    int_append(ic, 1);
+
+    int_push(ic, 0);
+
+    int_traverse(ic);
+    
+    return 0;
+}
